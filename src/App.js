a25b1ed2eb78c4f8a63fe7d607ac30abd7f2644a
DummyAccount0897 <69339042+DummyAccount0897@users.noreply.github.com>
2020-08-07 13:04:02 +0530

    Write a code where whatever you enter within a TextInput is displayed in a Text in same screen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from "react";
-import { Button, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, View, TextInput, Text } from "react-native";
 
-class App extends Component {
-  render() {
-    return (
-      <View style={styles.app}>
-        <Button onPress={() => {}} title="Example button" />
-      </View>
-    );
-  }
-}
+const App = () => {
+  const [textInputValue, setTextInputValue] = useState("Place holder");
+
+  return (
+    <View style={styles.app}>
+      <Button onPress={() => {}} title="Example button" />
+      <TextInput
+        style={styles.textInput}
+        value={textInputValue}
+        onChangeText={(textInputValue) => setTextInputValue(textInputValue)}
+      />
+      <Text>{textInputValue}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   app: {
     marginHorizontal: "auto",
     marginVertical: 20,
     maxWidth: 500
+  },
+  textInput: {
+    textColor: "black",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginVertical: 20,
+    padding: 4,
+    height: 40
   }
 });
 
